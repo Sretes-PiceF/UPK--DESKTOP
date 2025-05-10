8cebd0c90d89de622e4541808524ecd4ab6777f6
SingaBAung <7afajarihsan@gmail.com>
2025-05-10 14:39:57 +0700

    desain

---
+++ Desktop-Perpustakaan/Desktop-Perpustakaan/Data Siswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop_Perpustakaan
+{
+    public partial class Data_Siswa : UserControl
+    {
+        public Data_Siswa()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
